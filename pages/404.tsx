c40ea7a503e1be17f12290563aa49052256a21d7
Nhat K. (James) Nguyen <118421449+jimmynguyen1308@users.noreply.github.com>
2023-01-24 18:37:20 +1000

    Refactor single quotation to &apos;

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Page404() {
-  return <h1>Oops! This page isn't available right now.</h1>
+  return <h1>Oops! This page isn&apos;t available right now.</h1>
 }
